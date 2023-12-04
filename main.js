de523fbdea3c6f9eef48953e26353b7d25bed62f
manuel pisoni <manuelpisoni09@gmail.com>
2023-12-04 17:43:25 -0300

    hice un registro para que se muestren los helados que se fueron agregando

--- main.js
+++ main.js
@@ -2,55 +2,47 @@
 //  quiero que se muestre el stock de helados (a traves de un buscador)
 
 //  quiero registro de todos los helados agregados 
-let GustosHelados=[];
+let GustosHelados = [];
+let input = document.getElementById("helados");
+let boton = document.getElementById("agregar");
+let freezer = document.getElementById("freezer");
 
-let input = document.getElementById("helados")
-
-let boton = document.getElementById("agregar")
-
-let freezer = document.getElementById("freezer") 
-
-
-window.onload = function(){
-    let helados = localStorage.getItem("heladosguardados")
-    if (helados){
-
-        // SACAMOS DEL STORAGE EL OBJETO QUE HABIAMOS GUARDADO
-        let frozz = JSON.parse(helados)
-        
-        freezer.innerHTML=`Gusto: ${frozz.gusto}, Fecha:${frozz.fecha}  esto viene del frezzer`;
+window.onload = function() {
+    let helados = localStorage.getItem("heladosguardados");
+    if (helados) {
+        GustosHelados = JSON.parse(helados);
+        registro(); // Mostrar los helados almacenados al cargar la página
     }
 }
 
-
-
-boton.addEventListener("click",function(){
-
+boton.addEventListener("click", function() {
     let valorInput = input.value;
-
-
     let objeto = {
-        gusto:valorInput,
-        fecha:new Date().toLocaleString()
-    }
+        gusto: valorInput,
+        fecha: new Date().toLocaleString()
+    };
 
     // LE HAGO UN PUSH A LOS HELADOS Y LOS METO EN UNA ARRAY
-
     GustosHelados.push(objeto);
 
-
-// muestro los helados en el div 
+    // muestro los helados en el div
     freezer.innerHTML = `Gusto: ${objeto.gusto}, Fecha:${objeto.fecha}`;
 
-
-
-// guardamos un OBJETO en local storage
-    // localStorage.setItem("heladosguardados", JSON.stringify(objeto));
-// ahora guardamos el array 
+    // Guardar en localStorage después de cada clic
     localStorage.setItem("heladosguardados", JSON.stringify(GustosHelados));
 
-    input.value = ""
-} 
-)
+    input.value = "";
 
+    registro(); // Mostrar todos los helados después de agregar uno nuevo
+});
+
+let registro = () => {
+    
+    freezer.innerHTML = "";
+
+    // Mostrar todos los helados en el div
+    GustosHelados.forEach(function(objeto) {
+        freezer.innerHTML += `Gusto: ${objeto.gusto}, Fecha: ${objeto.fecha} Esto viene del frezzer<br>`;
+    });
+}
 
