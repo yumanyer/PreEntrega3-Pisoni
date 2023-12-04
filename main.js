0d13cbe28bd547a6a0e3b7a3f02d330b1d97b884
manuel pisoni <manuelpisoni09@gmail.com>
2023-12-04 17:23:47 -0300

    creo una array vacio y le voy metiendo los objetos y esta la guado en el local

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-//  CONTADOR DE STOCK DE HELADOS 
-
-
-//  quiero que se muestre que helado se esta sumando al stock 
 
 //  quiero que se muestre el stock de helados (a traves de un buscador)
 
 //  quiero registro de todos los helados agregados 
-
+let GustosHelados=[];
 
 let input = document.getElementById("helados")
 
@@ -18,10 +14,13 @@ let freezer = document.getElementById("freezer")
 window.onload = function(){
     let helados = localStorage.getItem("heladosguardados")
     if (helados){
-        freezer.innerHTML= helados
+
+        // SACAMOS DEL STORAGE EL OBJETO QUE HABIAMOS GUARDADO
+        let frozz = JSON.parse(helados)
+        
+        freezer.innerHTML=`Gusto: ${frozz.gusto}, Fecha:${frozz.fecha}  esto viene del frezzer`;
     }
 }
-
 
 
 
@@ -35,10 +34,20 @@ boton.addEventListener("click",function(){
         fecha:new Date().toLocaleString()
     }
 
+    // LE HAGO UN PUSH A LOS HELADOS Y LOS METO EN UNA ARRAY
+
+    GustosHelados.push(objeto);
+
+
+// muestro los helados en el div 
     freezer.innerHTML = `Gusto: ${objeto.gusto}, Fecha:${objeto.fecha}`;
 
 
-    localStorage.setItem("heladosguardados", valorInput);
+
+// guardamos un OBJETO en local storage
+    // localStorage.setItem("heladosguardados", JSON.stringify(objeto));
+// ahora guardamos el array 
+    localStorage.setItem("heladosguardados", JSON.stringify(GustosHelados));
 
     input.value = ""
 } 
